f365e86f92cce65b3a7c9bfd07b53dd056f4bdf2
Ahmad Haidar <ahmad.haidar.syr@gmail.com>
2025-07-20 18:40:42 +0300

    refactor: reorder event sub-routers to fix list owner events endpoints

--- app/api/routes/events/__init__.py
+++ app/api/routes/events/__init__.py
@@ -16,14 +16,16 @@ from app.api.routes.events import (
 
 router = APIRouter()
 
-# Include the sub-routers
+# fmt: off
+# Include the sub-routers - order matters for route precedence!
 router.include_router(create_event.router)
 router.include_router(list_events.router)
-router.include_router(get_event.router)
-router.include_router(delete_event.router)
 router.include_router(list_owner_events.router)
 router.include_router(list_participant_events.router)
+router.include_router(get_event.router)
+router.include_router(delete_event.router)
 router.include_router(event_add_participant.router)
 router.include_router(event_remove_participant.router)
 router.include_router(list_event_participants.router)
 router.include_router(update_event.router)
+# fmt: on
