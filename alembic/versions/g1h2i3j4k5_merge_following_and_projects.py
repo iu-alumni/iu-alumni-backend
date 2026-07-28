c0d9a1dae4ee8355da5e238b38161d4fccddcd17
G-Akleh <ghadeer_akleh@hotmail.com>
2026-07-28 23:48:54 +0300

    merge follow and notifications heads

--- alembic/versions/g1h2i3j4k5_merge_following_and_projects.py
+++ alembic/versions/g1h2i3j4k5_merge_following_and_projects.py
@@ -1,18 +1,15 @@
-"""Merge follow and projects heads
+"""Merge follow and notifications heads
 
 Revision ID: g1h2i3j4k5
-Revises: d1e2f3a4b5c6, f2a3b4c5d6e7
+Revises: f3a4b5c6d7e8, f2a3b4c5d6e7
 Create Date: 2026-07-22 00:00:00.000000
 
 """
 from typing import Sequence, Union
 
-from alembic import op
-
-
 # revision identifiers, used by Alembic.
 revision: str = 'g1h2i3j4k5'
-down_revision: Union[str, None] = ('d1e2f3a4b5c6', 'f2a3b4c5d6e7')
+down_revision: Union[str, None] = ('f3a4b5c6d7e8', 'f2a3b4c5d6e7')
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
